7d3c505dde50999a802a062030b616f89bee452d
Steve <stevedsimkins@gmail.com>
2024-02-02 07:19:30 -0500

    update button name

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -12,7 +12,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <title>This is frame 7</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/7.png" />
-    <meta property="fc:frame:button:1" content="Visit CosmicCowboys.cloud" />
+    <meta property="fc:frame:button:1" content="Cosmic Cowboys" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:button:2" content="Blog post Tutorial" />
     <meta property="fc:frame:button:2:action" content="post_redirect" />
